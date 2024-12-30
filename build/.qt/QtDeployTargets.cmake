e3ff9918097e1c600ae76dbf0b00cceda775bf33
Neshama1 <107024700+Neshama1@users.noreply.github.com>
2024-12-30 15:24:37 -0600

    Delete build directory

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_novalive_FILE /home/mike/Devel/GitHub/novalive/build/bin/novalive)
-set(__QT_DEPLOY_TARGET_novalive_TYPE EXECUTABLE)
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mike/Devel/GitHub/novalive/src
-# Build directory: /home/mike/Devel/GitHub/novalive/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
